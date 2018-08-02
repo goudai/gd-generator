f4e2910ef47b3e698fa7b6511f0d5043e6a2372a
freeman <247687009@qq.com>
2018-08-02 14:43:32 +0800

    一波更新解决databug

--- gd-generator-default/src/main/java/io/gd/generator/Generator.java
+++ gd-generator-default/src/main/java/io/gd/generator/Generator.java
@@ -1,21 +1,24 @@
 package io.gd.generator;
 
 import io.gd.generator.handler.Handler;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Arrays;
 import java.util.Objects;
 
+
+@Slf4j
 public class Generator {
 
-	public static void generate(final Config config, Handler... hanlderClasses) throws Exception {
-		Objects.requireNonNull(hanlderClasses, "handler classes must not be null");
-		Arrays.stream(hanlderClasses).forEach(handler -> {
-			try {
-				handler.start(config);
-			} catch (Exception e) {
-				logger.error(e.getMessage(),e);();
-			}
-		});
-	}
+    public static void generate(final Config config, Handler... hanlderClasses) {
+        Objects.requireNonNull(hanlderClasses, "handler classes must not be null");
+        Arrays.stream(hanlderClasses).forEach(handler -> {
+            try {
+                handler.start(config);
+            } catch (Exception e) {
+                log.error(e.getMessage(), e);
+            }
+        });
+    }
 
 }
